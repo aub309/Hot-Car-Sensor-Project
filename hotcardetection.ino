d543e68767a8d8216f7d1c9fb0c588fc73df43c3
aub309 <35350430+aub309@users.noreply.github.com>
2018-03-29 13:27:18 -0400

    Add files via upload

--- hotcardetection.ino
+++ hotcardetection.ino
@@ -5,10 +5,10 @@
 //As of now, parts for the pressure, motion, and temperature sensors, and the window opener have arrived. This is what we have as of now 
 //New parts have been added to the code, for detecting pressure and CO2, as well as for opening the windows. 
 //Also the code has been recently edited to add detections for the temperature thresholds.
-//This time my group was unable to meet due to the snow. However, I was able to add code for detecting battery life and if the texting number has been set or not.
+//This time, I was able to allow it to connect to the SIM Card and allow it to send text messages
 
 #include <GSM.h> //Needed for sending the text message and 911 call
-#define PINNUMBER "0" //PIN number for the SIM Card used in the device
+#define PINNUMBER "1234" //PIN number for the SIM Card used in the device
 
 //Variables for new GSM Shield input
 const int led_pin = 8;
@@ -20,6 +20,10 @@ GSMPIN PINManager; //For the GSM Shield
 GSM gsmAccess; //For the phone functions
 GSM_SMS sms; //For the text message part
 GSMVoiceCall vcs; //For the phone call part for phoning 911
+
+//SoftwareSerial GPRS(7, 8);
+unsigned char buffer[64]; // buffer array for data recieve over serial port
+int count=0;     // counter for buffer array 
 
 const int TEMP0 = 0;   //Temperature Sensor is on pin A0
 int CO2 = 1;    //CO2 Sensor is on pin 1
@@ -34,44 +38,43 @@ const int mid_bound1 = 85; //Middle bound 1 for the temperature detection
 const int mid_bound2 = 90; //Middle bound 2 for the temperature detection 
 const int upper_bound = 95; //Upper bound for the temperature detection 
 
-int val = 0;     //Variable to hold analog reading. 
-int valmotion1 = 0;  //Variable to hold motion reading of first motion sensor
-int valmotion2 = 0;  //Variable to hold motion reading of second motion sensor
-int pdetect = 0;     //Variable to hold voltage of the pressure sensor
-int co2detect = 0;   //Variable to hold voltage of the CO2 sensor
-int batterydetect = 0;   //Variable to hold voltage of battery life
+int val;     //Variable to hold analog reading. 
+int valmotion1;  //Variable to hold motion reading of first motion sensor
+int valmotion2;  //Variable to hold motion reading of second motion sensor
+int pdetect;     //Variable to hold voltage of the pressure sensor
+int co2detect;   //Variable to hold voltage of the CO2 sensor
+int batterydetect;   //Variable to hold voltage of battery life
 
-boolean motion1 = false; //Variable for the first Motion Detection
-boolean motion2 = false; //Variable for the second Motion Detection
-boolean pressure = false; //Variable for the pressure detection
-boolean co2high = false;  //Variable for the CO2 detection
+boolean motion1; //Variable for the first Motion Detection
+boolean motion2; //Variable for the second Motion Detection
+boolean pressure; //Variable for the pressure detection
+boolean co2high;  //Variable for the CO2 detection
 
 String call911 = "17179820203";  // variable for the 911 call, currently wired to my number as to keep from accidentally phoning the police
-char call911buffer[11]; //Converts the call911 into Char Array for it to be used in vcs
+char call911buffer[20]; //Converts the call911 into Char Array for it to be used in vcs
 String smsnumber = "17179820203"; //Variable for the social message number
-char smsbuffer[11]; //Converts the smsnumber into Char Array for it to be used in vcs
+char smsbuffer[20]; //Converts the smsnumber into Char Array for it to be used in vcs
+
+//Variables to make the emergency actions only run once, and not repeatedly.
+boolean sentmessage1;
+boolean sentmessage2;
+boolean soundedalarm;
+boolean windowsdown;
+boolean calledemergency;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(motionDetect1, INPUT);  // declare motionDetect1 as input
-  pinMode(motionDetect2, INPUT);  // declare motionDetect2 as input
-  pinMode(A1, INPUT); //For Pressure Input
-  pinMode(A2, INPUT); //For CO2 Input
-   while (!Serial) {
-     ; // wait for serial port to connect. Needed for native USB port only
-   }
-   pinMode(8, OUTPUT);  // sets the digital pin 8 as output for the window opener
+  Serial.begin(9600);    
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("SMS Messages Sender");
 
-   Serial.println("SMS Messages Sender");
-
-   // connection state
-   boolean notConnected = true;
-  // put your setup code here, to run once:
-
+  // connection state
+  boolean notConnected = true;
   //Connect to the GSM Shield
   while (notConnected)
   {
-    if (gsmAccess.begin(PINNUMBER) == GSM_READY)
+    if (gsmAccess.begin() == GSM_READY)
       notConnected = false;
     else
     {
@@ -81,6 +84,31 @@ void setup() {
   }
 
   Serial.println("GSM initialized");
+
+  pinMode(motionDetect1, INPUT);  // declare motionDetect1 as input
+  pinMode(motionDetect2, INPUT);  // declare motionDetect2 as input
+  pinMode(A1, INPUT); //For Pressure Input
+  pinMode(A2, INPUT); //For CO2 Input
+  pinMode(8, OUTPUT);  // sets the digital pin 8 as output for the window opener
+
+  //Set all emergency actions to false
+  sentmessage1 = false;
+  sentmessage2 = false;
+  soundedalarm = false;
+  windowsdown = false;
+  calledemergency = false;
+
+  val = 0;     //Variable to hold analog reading. 
+  valmotion1 = 0;  //Variable to hold motion reading of first motion sensor
+  valmotion2 = 0;  //Variable to hold motion reading of second motion sensor
+  pdetect = 0;     //Variable to hold voltage of the pressure sensor
+  co2detect = 0;   //Variable to hold voltage of the CO2 sensor
+  batterydetect = 0;   //Variable to hold voltage of battery life
+
+  motion1 = false; //Variable for the first Motion Detection
+  motion2 = false; //Variable for the second Motion Detection
+  pressure = false; //Variable for the pressure detection
+  co2high = false;  //Variable for the CO2 detection
 }
 
 void loop() {
@@ -154,7 +182,7 @@ void loop() {
     }
 
     //Pressure Sensor
-    if (pdetect > 1000) {
+    if (pdetect > 700) {
       pressure = true;
       Serial.println("Pressure detected!");
     }
@@ -176,14 +204,18 @@ void loop() {
       if (co2high == true) { //Test for CO2
         Serial.println("CO2 concentration high!");
         //Send Emergency Message to phone of the user. 
-        if (smsnumber.equals("")) { //In case the user has not set their phone number yet
-          sms.print("No phone number set. Message send failure.");
-        }
-        else { //If the phone number has been set
-          sms.beginSMS(smsbuffer);
-          sms.print("Alert, your child or pet is in danger in your vehicle. Please attend to them as soon as possible.");
-          sms.endSMS();
-          Serial.println("\nMessage Sent!\n");
+        if (sentmessage1 == false) {  //Code to make the emergency action not repeatedly run when threshold is hit
+          if (smsnumber.equals("")) { //In case the user has not set their phone number yet
+            Serial.println("No phone number set. Message send failure.");
+          }
+          else { //If the phone number has been set
+            Serial.println("\nSending Message\n");
+            sms.beginSMS("17179820203");
+            sms.print("Alert, your child or pet is in danger in your vehicle. Please attend to them as soon as possible.");
+            sms.endSMS();
+            Serial.println("\nMessage Sent!\n");
+          }
+          sentmessage1 = true; //Set variable after it has run to true.
         }
       }
       
@@ -191,24 +223,33 @@ void loop() {
       if (lower_bound <= degreesF) { //Test for above 80 degrees
         Serial.println("Temperature threshold 1");
          //Send Emergency Message to phone of the user. 
-        if (smsnumber.equals("")) { //In case the user has not set their phone number yet
-          sms.print("No phone number set. Message send failure.");
-        }
-        else { //If the phone number has been set
-          sms.beginSMS(smsbuffer);
-          sms.print("Alert, your child or pet is in danger in your vehicle. Please attend to them as soon as possible.");
-          sms.endSMS();
-          Serial.println("\nMessage Sent!\n");
-        }
+
+         if (sentmessage2 == false) {  //Code to make the emergency action not repeatedly run when threshold is hit
+          if (smsnumber.equals("")) { //In case the user has not set their phone number yet
+            Serial.println("No phone number set. Message send failure.");
+          }
+          else { //If the phone number has been set
+            Serial.println("\nSending Message\n");
+            sms.beginSMS("17179820203");
+            sms.print("Alert, your child or pet is in danger in your vehicle. Please attend to them as soon as possible.");
+            sms.endSMS();
+            Serial.println("\nMessage Sent!\n");
+            
+          }
+          sentmessage2 = true; //Set variable after it has run to true.
+         }
 
         if (mid_bound1 <= degreesF) { //Test for above 85 degrees
           Serial.println("Temperature threshold 2");
           //Open the windows of the car
-
-          digitalWrite(8, HIGH);   // sets the digital pin 8 on for the windows
-          delay(2000);             // waits for two seconds for it to move
-          digitalWrite(8, LOW);    // sets the digital pin 8 off for the windows
-          delay(2000);    
+          
+          if (windowsdown == false){  //Code to make the emergency action not repeatedly run when threshold is hit
+            digitalWrite(8, HIGH);   // sets the digital pin 8 on for the windows
+            delay(2000);             // waits for two seconds for it to move
+            digitalWrite(8, LOW);    // sets the digital pin 8 off for the windows
+            delay(2000);
+            windowsdown = true;  //Set variable after it has run to true.
+          }
           
           if (mid_bound2 <= degreesF) { //Test for above 90 degrees
             Serial.println("Temperature threshold 3");
@@ -216,10 +257,14 @@ void loop() {
 
             if (upper_bound <= degreesF) { //Test for above 95 degrees
               Serial.println("Temperature threshold 4");
-              //TODO code for the last threshold once parts arrive
-              vcs.voiceCall(call911buffer); //Phone 911 here
+
+              if (calledemergency == false) { //Code to make the emergency action not repeatedly run when threshold is hit
+                //TODO code for the last threshold once parts arrive
+                vcs.voiceCall(call911buffer); //Phone 911 here
       
-              vcs.hangCall(); //Hang up 911
+                vcs.hangCall(); //Hang up 911
+                calledemergency = true; //Set variable after it has run to true.
+              }
             }
           }
         }     
@@ -233,9 +278,7 @@ void loop() {
     else {
       Serial.println("Standby");
     }
-
-    // put your main code here, to run repeatedly:
-
+    
     delay(1000); // used to make it repeat once per second
   }
 
@@ -276,4 +319,3 @@ void BatteryVoltage() //Function to test the battery juice left
     Serial.println("Battery Level sufficient.");
   }
 }
-
